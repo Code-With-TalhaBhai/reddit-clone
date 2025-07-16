53c48d55632374f025ed2d7add72ada8ed75d5a8
= <=>
2025-07-17 00:47:46 +0500

    fixing deployment issues of refactored app 3

--- apollo-client/index.ts
+++ apollo-client/index.ts
@@ -1,11 +1,10 @@
 import { ApolloClient,InMemoryCache} from "@apollo/client";
 
+const basePath = process.env.NODE_ENV === 'production' 
+    ? 'https://reddit-clone-omega-eight.vercel.app' 
+    : 'http://localhost:3000';
+
 export const client = new ApolloClient({
-    // uri: 'https://48p1r2roz4.sse.codesandbox.io',
-    // uri: 'https://public095fff4d7b479e37.stepzen.net/api/yucky-quetzal/__graphql',
-    uri: 'http://localhost:3000/api/apollo',
-    // headers:{
-    //   Authorization: `Apikey ${process.env.STEPZEN_APIKEY}`
-    // },
+    uri: `${basePath}/api/apollo`,
     cache: new InMemoryCache()
   });
