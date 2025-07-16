85c14bdd978f70cbaf63f7b736913327feb23da7
= <=>
2025-07-17 00:24:36 +0500

    fixing deployment issues of refactored app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,17 +5,18 @@ import Header from '../components/Header'
 import { ApolloProvider } from '@apollo/client'
 import { client } from '../apollo-client'
 import { Toaster } from 'react-hot-toast'
-import { db } from '../drizzle_schema/client'
-import { pgTable, serial, text, timestamp, varchar } from 'drizzle-orm/pg-core'
-import { sql } from 'drizzle-orm'
-import { useEffect } from 'react'
+import { Session } from 'next-auth'
 
 
-function MyApp({ Component, pageProps:{session, ...pageProps} }: AppProps) {
+
+// function MyApp({ Component, pageProps:{session, ...pageProps} }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps & { pageProps: { session: Session | null } }) {
+  const basePath = process.env.NODE_ENV === 'production' 
+    ? 'https://reddit-clone-omega-eight.vercel.app/api/auth' 
+    : '/api/auth';
 
   return(
-    <SessionProvider session={session} basePath="https://reddit-clone-omega-eight.vercel.app/api/auth">
-    {/* <SessionProvider session={session} basePath="/api/auth"> */}
+    <SessionProvider session={pageProps.session} basePath={basePath}>
     <ApolloProvider client={client}>
       <div className='h-screen overflow-y-scroll bg-slate-200'>
         <Toaster/>
